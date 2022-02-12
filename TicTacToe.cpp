9d7892c7278c7c70ac3e440ec5ed833b482388d9
tylerkimbell12 <tyler@kimbellhouse.net>
2022-02-11 17:17:07 -0700

    Implemented DisplayBoard Function

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -10,6 +10,19 @@ void CreateBoard(vector<int> &b){
     }
 }
 
+void DisplayBoard(vector<int> &b){
+    cout<< b[0];
+    cout<< b[1];
+    cout<< b[2] << endl;
+    cout<< b[3];
+    cout<< b[4];
+    cout<< b[5] << endl;
+    cout<< b[6];
+    cout<< b[7];
+    cout<< b[8] << endl;
+
+}
+
 int main(){
     vector<int> board;
     CreateBoard(board);
